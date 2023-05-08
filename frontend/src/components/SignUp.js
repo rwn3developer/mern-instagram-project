1be1a477ea0b7416f8674b6e94c58f26e28baafe
milansir <rwn3developer11@gmail.com>
2023-05-08 16:23:57 +0530

    instagram-clone

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const SignUp = () => {
 
     const fetchData = async() => {
-        const response = await fetch("http://localhost:9000/"); //data fetch
+        const response = await fetch("http://localhost:9000/"); 
         const data = await response.json();
         console.log(data);
     }
