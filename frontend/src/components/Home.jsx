6db9448740d932b94ebbb3024ac501b4bfd8069f
milansir <rwn3developer11@gmail.com>
2023-05-11 15:58:19 +0530

    homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,38 @@
+import "./Home.css";
 const Home = () => {
     return (
-        <h1>Home page</h1>
+        <div className="home">
+            {/* card */}
+            <div className="card">
+                {/* card header  */}
+                <div className="card-header">
+                    <div className="card-pic">
+                        <img width={100} src="https://images.pexels.com/photos/3775553/pexels-photo-3775553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    </div>
+                    <h5>jay mataji</h5>
+                </div>
+                {/* card-image */}
+                <div className="card-image">
+                    <img width={100} src="https://images.pexels.com/photos/6235052/pexels-photo-6235052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </div>
+                {/* card-content */}
+                <div className="card-content">
+                    <span class="material-symbols-outlined">
+                        favorite
+                    </span>
+                    <p>1 Like</p>
+                    <p>This is amazing</p>
+                </div>
+                {/* add-comment */}
+                <div className="add-comment">
+                    <span class="material-symbols-outlined">
+                        sentiment_satisfied
+                    </span>
+                    <input type="text" placeholder="add a comment" />
+                    <button className="comment">Post</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import { Link,useNavigate } from 'react-router-dom';
 import './SignUp.css'
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import { toast } from 'react-toastify';
 const SignUp = () => {
     const navigate = useNavigate();
