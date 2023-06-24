c03530467aebee079c51aea1a570a5aba6d5f0ad
milansir <rwn3developer11@gmail.com>
2023-06-24 12:42:34 +0530

    mongo-connect

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://rwn3developer11:eLx2eo8nctrrumg2@cluster0.tr84nrb.mongodb.net/");
 
+//mongo connection
 mongoose.connection.on("connected",()=>{
     console.log("successfully connected mongo");
 })
